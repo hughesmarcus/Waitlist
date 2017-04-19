5852977c79051a2b6274071ee3bc45f5fd3f4a16
Marcus Hughes <marcushughes80@gmail.com>
2017-04-19 12:14:29 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         // Create an item touch helper to handle swiping items off the list
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 
-            // COMPLETED (4) Override onMove and simply return false inside
+            
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
                 //do nothing, we only care about swiping
@@ -71,13 +71,13 @@ public class MainActivity extends AppCompatActivity {
     
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
-                // COMPLETED (8) Inside, get the viewHolder's itemView's tag and store in a long variable id
+                
                 //get the id of the item being swiped
                 long id = (long) viewHolder.itemView.getTag();
                 // COMPLETED (9) call removeGuest and pass through that id
                 //remove from DB
                 removeGuest(id);
-                // COMPLETED (10) call swapCursor on mAdapter passing in getAllGuests() as the argument
+               
                 //update the list
                 mAdapter.swapCursor(getAllGuests());
             }
